41ae56ab95f63fe0d9a3806e8b937337b5ea3080
cristiano <cristiano.lourenco.ao@gmail.com>
2023-11-11 10:31:30 +0100

    displaying the right category

--- lib/screen/category_screen.dart
+++ lib/screen/category_screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/model/category_model.dart';
+import 'package:meals_app/model/meal_model.dart';
 import 'package:meals_app/screen/meals_screen.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, CategoryModel model) {
+    final mealsList = dummyMealModels
+        .where(
+          (meal) => meal.categories.contains(model.id),
+        )
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => const MealsScreen(
-          title: 'Some title',
-          mealList: [],
+        builder: (context) => MealsScreen(
+          title: model.title,
+          mealList: mealsList,
         ),
       ),
     );
@@ -35,7 +43,9 @@ class CategoryScreen extends StatelessWidget {
           for (final categoryModel in availableCategories)
             CategoryGridItem(
               categoryModel: categoryModel,
-              onSelectCategory: () => _selectCategory(context),
+              onSelectCategory: () {
+                _selectCategory(context, categoryModel);
+              },
             )
         ],
       ),
